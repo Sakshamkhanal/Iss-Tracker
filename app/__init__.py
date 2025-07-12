56f0e96e18bd40b5e982171f83896c9f75a2970d
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-12 22:19:17 +0545

    Removed unwanted files and setting up use of celery

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,14 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    app.config.from_mapping(
+        CELERY=dict(
+            broker_url='redis://localhost',
+            result_backend="redds://localhost",
+            task_ignore_result=True.
+            ),
+            )
+
 
     from app.get_gps import get_gps as main_get_gps
     app.register_blueprint(main_get_gps)
@@ -15,4 +21,5 @@ def create_app(config_class=Config):
     #def test_page():
     #   return '<h1>Testing the Flask APplication Factory Pattern</h1>'
 
+    celery_app = celery_init_app(app)
     return app
---
+++ app/tasks.py
@@ -0,0 +1,13 @@
+ import requests
+ import csv
+ from celery import Celery
+ CELERY_BROKER_URL = 'redis://localhost:6379/0'
+ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+ celery = Celery('tasks',broker=CELERY_BROKER_URL,backend=CELERY_RESULT_BACKEND)
+
+@celery.task
+def get_satellites():
+    url = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=visual&FORMAT=json'
+    data = requests.get(url)
+    return data
