5a74c5aaa699325c1f1c5b45ff99b212d6e834fd
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-16 18:40:45 +0545

    Added models to store tle

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from extensions import db
+
+class SatelliteTLE(db.Model):
+    __tablename__ = 'satellite_tles'
+
+    id = db.Column(db.Integer,primary=True)
+    object_name = db.Column(db.String(128))
+    object_id = db.Column(db.String(32))
+    epoch = db.Column(db.String(32))
+    mean_motion = db.Column(db.Float)
+    eccenetricity = db.Column(db.Float)
+    inclination = db.Column(db.Float)
+    ra_of_asc_node = db.Column(db.Float)
+    mean_anomaly = db.Column(db.Float)
+    classification_type = db.Column(db.String(8))
+    norad_cat_id = db.Column(db.Integer,index=True)
+    element_set_no = db.Column(db.Integer)
+    rev_at_epoch = db.Column(db.Integer)
+    bstar = db.Column(db.Float)
+    mean_motion_dot = db.Column(db.Float)
+    mean_motion_ddot = db.Column(db.Float)
