e127d3bf33104715140609de11c183f0627cb322
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-17 20:00:12 +0545

    made connection to postgres

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URI"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = "postgresql://user_saksham:5456@localhost/iss_tracking"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     celery_broker_url =  'redis://localhost:6379/0'
     celery_result_backend = 'redis://localhost:6379/0'
